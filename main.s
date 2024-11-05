1753b8b7493295d35ea9452cf90057265738933a
Florescu <pf522@ic.ac.uk>
2024-11-05 11:57:28 +0000

    starting to build code

--- main.s
+++ main.s
@@ -9,8 +9,25 @@ main:
 	org	0x100		    ; Main code starts here at address 0x100
 start:
 	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
+	movwf	TRISC, A	    ; Port C all outputs
+	movwf	TRISD, A	    ; Port D set to 0, this is clock pulse
+	movwf	LATC, A		    ; Set outout to 0
+setup:
+	movlw	0x5
+	movwf	0x20, A
+up_loop: 
+	incf	LATC, F
+	decf	0x20
+	;call delay
+	btfss	LATC, 2		    ; Check bit 2 is 1 if it is skip
+	goto	up_loop
+	
+down_loop:
+	decf	LATC, F
+	;call delay
+	btfss	LATC, 0		    ;check if value is 0
+	goto	down_loop
+	
 loop:
 	movff 	0x06, PORTB
 	incf 	0x06, W, A
