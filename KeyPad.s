4ec86afb5a10975405aae5f2ffb614ea0838ec8d
Florescu <pf522@ic.ac.uk>
2024-11-19 10:20:14 +0000

    almost done keypad

--- KeyPad.s
+++ KeyPad.s
@@ -6,17 +6,16 @@ psect	udata_acs   ; reserve data space in access ram
 KeyPad_counter: ds    1	    ; reserve 1 byte for variable KeyPad_counter
 KeyPad_Row: ds 1             ; reserve 1 byte for row scanning
 KeyPad_Col: ds 1             ; reserve 1 byte for column scanning
-KeyPad_RowCol: ds 1
+keyval: ds 1
     
 psect	uart_code,class=CODE
     
-KeyPad_Setup:
-    bsf	    BSR, 4	;sets the 4th bit to high which selects bank 4
-    bsf	    REPU	;enables the pill-ups to on for PORTE (bits 0 - 3 set as high unless a button is pressed 
-    clrf    LATE	;set all of Port E as an output 
-    movlw   0x0F
-    movwf   TRISE	; Set PORTE 4-7 as outputs and 0-3 as inputs (makes call collumns 0) 
-    clrf    LATF
+KeyPad_setup:
+    movlb 15; go to access bank 15
+    bsf REPU; pull-up the byte in access bank 15
+    movlb 0; for cleanliness
+    clrf LATE; we want LATCHE to be cleared throughout
+    clrf TRISD
     return
 
     
@@ -38,155 +37,85 @@ Read_Key:
     iorwf   KeyPad_Col, W   ; Perform OR with the column state (combines the states)
     movwf   KeyPad_RowCol   ; Store the combined row and column state in KeyPad_RowCol
     
-   movf    KeyPad_RowCol, W
+   movf    keyval, W
 
-   btfss   KeyPad_Row, 0            ; Test bit 0 of RowState for row 0
-    bra     Row0_Active            ; If bit 0 is clear (0), row 0 is active
-
-    ; Check for the second row (RowState = 0x02 means row 1 is active)
-    btfss   KeyPad_Row, 1            ; Test bit 1 of RowState for row 1
-    bra     Row1_Active            ; If bit 1 is clear (0), row 1 is active
-
-    ; Check for the third row (RowState = 0x04 means row 2 is active)
-    btfss   KeyPad_Row, 2            ; Test bit 2 of RowState for row 2
-    bra     Row2_Active            ; If bit 2 is clear (0), row 2 is active
-
-    ; Check for the fourth row (RowState = 0x08 means row 3 is active)
-    btfss   KeyPad_Row, 3            ; Test bit 3 of RowState for row 3
-    bra     Row3_Active            ; If bit 3 is clear (0), row 3 is active
-
-    bra	    Nada		;nothing is pressed
+Combo_tests: ; iteratively go through each of the 16 combinations until the value in the keyval register matches with the one being tested
+    movlw 0xFF ; i.e. if no value has been pressed, stay within this loop until no longer true 
+    cpfseq keyval, A ; compare value in keyval with W, store result in A
+    bra test_0 ; move on to next test if not equal
+    retlw 0x00; clear W
     
-Row0_Active:
-    btfss   KeyPad_Col, 4            ; Check if column 0 is active (bit 0 of ColState)
-    bra     Row0_Col0_Pressed      ; If bit 0 of ColState is clear (0), column 0 is pressed
+test_0: ;0111 0111
+    movlw 0x77 ; CHECK
+    cpfseq keyval, A 
+    bra test_1
+    retlw 0x77 ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-    btfss   KeyPad_Col, 5            ; Check if column 1 is active (bit 1 of ColState)
-    bra     Row0_Col1_Pressed      ; If bit 1 of ColState is clear (0), column 1 is pressed
+test_1: ;0111 1011
+    movlw 0x7B ; CHECK
+    cpfseq keyval, A 
+    bra test_2
+    retlw 0x7B;REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-    btfss   KeyPad_Col, 6            ; Check if column 2 is active (bit 2 of ColState)
-    bra     Row0_Col2_Pressed      ; If bit 2 of ColState is clear (0), column 2 is pressed
+test_2: ;0111 1101
+    movlw 0x7D ; CHECK
+    cpfseq keyval, A 
+    bra test_3
+    retlw 0x7D ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-    btfss   KeyPad_Col, 7            ; Check if column 3 is active (bit 3 of ColState)
-    bra     Row0_Col3_Pressed      ; If bit 3 of ColState is clear (0), column 3 is pressed
+test_3: ;0111 1110
+    movlw 0x7E ; CHECK
+    cpfseq keyval, A 
+    bra test_4
+    retlw 0x7E ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-Row1_Active:
-    btfss   KeyPad_Col, 4            ; Check if column 0 is active (bit 0 of ColState)
-    bra     Row1_Col0_Pressed      ; If bit 0 of ColState is clear (0), column 0 is pressed
-
-    btfss   KeyPad_Col, 5            ; Check if column 1 is active (bit 1 of ColState)
-    bra     Row1_Col1_Pressed      ; If bit 1 of ColState is clear (0), column 1 is pressed
-
-    btfss   KeyPad_Col, 6            ; Check if column 2 is active (bit 2 of ColState)
-    bra     Row1_Col2_Pressed      ; If bit 2 of ColState is clear (0), column 2 is pressed
-
-    btfss   KeyPad_Col, 7            ; Check if column 3 is active (bit 3 of ColState)
-    bra     Row1_Col3_Pressed      ; If bit 3 of ColState is clear (0), column 3 is pressed
-
-Row2_Active:
-    btfss   KeyPad_Col, 4            ; Check if column 0 is active (bit 0 of ColState)
-    bra     Row2_Col0_Pressed      ; If bit 0 of ColState is clear (0), column 0 is pressed
-
-    btfss   KeyPad_Col, 5            ; Check if column 1 is active (bit 1 of ColState)
-    bra     Row2_Col1_Pressed      ; If bit 1 of ColState is clear (0), column 1 is pressed
-
-    btfss   KeyPad_Col, 6            ; Check if column 2 is active (bit 2 of ColState)
-    bra     Row2_Col2_Pressed      ; If bit 2 of ColState is clear (0), column 2 is pressed
-
-    btfss   KeyPad_Col, 7            ; Check if column 3 is active (bit 3 of ColState)
-    bra     Row2_Col3_Pressed      ; If bit 3 of ColState is clear (0), column 3 is pressed
-
-Row3_Active:
-    btfss   KeyPad_Col, 4            ; Check if column 0 is active (bit 0 of ColState)
-    bra     Row3_Col0_Pressed      ; If bit 0 of ColState is clear (0), column 0 is pressed
-
-    btfss   KeyPad_Col, 5            ; Check if column 1 is active (bit 1 of ColState)
-    bra     Row3_Col1_Pressed      ; If bit 1 of ColState is clear (0), column 1 is pressed
-
-    btfss   KeyPad_Col, 6            ; Check if column 2 is active (bit 2 of ColState)
-    bra     Row3_Col2_Pressed      ; If bit 2 of ColState is clear (0), column 2 is pressed
-
-    btfss   KeyPad_Col, 7            ; Check if column 3 is active (bit 3 of ColState)
-    bra     Row3_Col3_Pressed      ; If bit 3 of ColState is clear (0), column 3 is pressed
-
+test_4: ;1011 0111
+    movlw 0xB7 ; CHECK
+    cpfseq keyval, A 
+    bra test_5
+    retlw 0xB7 ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
     
+test_5: ;0111 1011
+    movlw 0xBB ; CHECK
+    cpfseq keyval, A 
+    bra test_6
+    retlw 0xBB ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
     
-Row0_Col0_Pressed:
-    movwf   LATG
-    return
-Row0_Col1_Pressed:
-    movwf   LATG
-    return
-Row0_Col2_Pressed:
-    movwf   LATG
-    return
-Row0_Col3_Pressed:
-    movwf   LATG
-    return
-Row1_Col0_Pressed:
-    movwf   LATG
-    return
-Row1_Col1_Pressed:
-    movwf   LATG
-    return
-Row1_Col2_Pressed:
-    movwf   LATG
-    return
-Row1_Col3_Pressed:
-    movwf   LATG
-    return
-Row2_Col0_Pressed:
-    movwf   LATG
-    return
-Row2_Col1_Pressed:
-    movwf   LATG
-    return
-Row2_Col2_Pressed:
-    movwf   LATG
-    return
-Row2_Col3_Pressed:
-    movwf   LATG
-    return
-Row3_Col0_Pressed:
-    movwf   LATG
-    return
-Row3_Col1_Pressed:
-    movwf   LATG
-    return
-Row3_Col2_Pressed:
-    movwf   LATG
-    return
-Row3_Col3_Pressed:
-    movwf   LATG
-    return
-Nada:
-    movwf   LATG
-    return
-    
+test_6: ;0111 1101
+    movlw 0xBD ; CHECK
+    cpfseq keyval, A 
+    bra test_7
+    retlw 0xBD ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-  
-  
-    
-    
-    
-  
-    
-    
-KeyPad_Transmit_Message:	    ; Message stored at FSR2, length stored in W
-    movwf   KeyPad_counter, A
-KeyPad_Loop_message:
-    movf    POSTINC2, W, A
-    call    KeyPad_Transmit_Byte
-    decfsz  KeyPad_counter, A
-    bra	    KeyPad_Loop_message
-    return
+test_7: ;0111 1110
+    movlw 0xBE ; CHECK
+    cpfseq keyval, A 
+    bra test_8
+    retlw 0xBE ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
 
-KeyPad_Transmit_Byte:	    ; Transmits byte stored in W
-    btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
-    bra	    KeyPad_Transmit_Byte
-    movwf   TXREG1, A
-    return
+test_8: ;1101 0111
+    movlw 0xD7 ; CHECK
+    cpfseq keyval, A 
+    bra test_1
+    retlw 0xD7 ; REPLACE WITH APPROPRIATE ASCII CHARACTER!
+    
+;KeyPad_Transmit_Message:	    ; Message stored at FSR2, length stored in W
+;    movwf   KeyPad_counter, A
+;KeyPad_Loop_message:
+;    movf    POSTINC2, W, A
+ ;   call    KeyPad_Transmit_Byte
+  ;  decfsz  KeyPad_counter, A
+   ; bra	    KeyPad_Loop_message
+    ;return
+
+;KeyPad_Transmit_Byte:	    ; Transmits byte stored in W
+ ;   btfss   TX1IF	    ; TX1IF is set when TXREG1 is empty
+  ;  bra	    KeyPad_Transmit_Byte
+   ; movwf   TXREG1, A
+    ;return
 
 Delay:
     movlw   0xFF
+    
+    end
 
--- main.s
+++ main.s
@@ -25,56 +25,38 @@ rst: 	org 0x0
 	; ******* Programme FLASH read Setup Code ***********************
 setup:	bcf	CFGS	; point to Flash program memory  
 	bsf	EEPGD 	; access Flash program memory
-	call	KeyPad_Setup	; setup KeyPad
-	;call	LCD_Setup	; setup LCD
+	call	UART_Setup	; setup UART
+	call	LCD_Setup	; setup UART
+	call	KeyPad_setup
 	goto	start
 	
-	; ******* Main programme **************************************** 	;lfsr	0, myArray	; Load FSR0 with address in RAM	
-	;movlw	low highword(myTable)	; address of data in PM
-	;movwf	TBLPTRU, A		; load upper bits to TBLPTRU
-	;movlw	high(myTable)	; address of data in PM
-	;movwf	TBLPTRH, A		; load high byte to TBLPTRH
-	;movlw	low(myTable)	; address of data in PM
-	;movwf	TBLPTRL, A		; load low byte to TBLPTRL
-	;movlw	myTable_l	; bytes to read
-	;movwf 	counter, A		; our counter register
-	;tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	;movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	;decfsz	counter, A		; count down to zero
-	;bra	loop		; keep going until finished
+	; ******* Main programme ****************************************
+start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
+	movlw	low highword(myTable)	; address of data in PM
+	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH, A		; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL, A		; load low byte to TBLPTRL
+	movlw	myTable_l	; bytes to read
+	movwf 	counter, A		; our counter register
+loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop		; keep going until finished
 		
-	;movlw	myTable_l	; output message to KeyPad
-	;lfsr	2, myArray
-	;call	KeyPad_Transmit_Message
+	movlw	myTable_l	; output message to UART
+	lfsr	2, myArray
+	call	UART_Transmit_Message
 
-	;movlw	myTable_l	; output message to LCD
-	;addlw	0xff		; don't send the final carriage return to LCD
-	;lfsr	2, myArray
-	;call	LCD_Write_Message
-	
-	; call	delay		;call routine to clear the display
-	; call	LCD_Clear_Display   
-	
-	
-	;call	LCD_Set_Cursor	    ;set cursor to second line
-	;movlw	myTable_l	    ;
-	;addlw	0xff
-	;lfsr	2, myArray
-	;call	LCD_Write_Message
-	
-	
-	;goto	$		; goto current line in code
+	movlw	myTable_l	; output message to LCD
+	addlw	0xff		; don't send the final carriage return to LCD
+	lfsr	2, myArray
+	call	LCD_Write_Message
+	call	KeyPad_main
+	goto	$		; goto current line in code
 
-start:
-	call Read_Key  ; Call Read_Key until a key is pressed
-	bra loop
-
-loop:  
-	call Read_Key  ; Continuously call Read_Key in the main loop
-	bra loop      ; Repeat loop
-	
 	; a delay subroutine if you need one, times around loop in delay_count
-	
 delay:	decfsz	delay_count, A	; decrement until zero
 	bra	delay
 	return
